03e4edb9a9af71e4a64e5f1377a3b0639265ada9
Jami Moubry <jami@moubry.com>
2022-02-19 09:02:42 -0600

    Create initial backend and client

---
+++ src/backend/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Define collection and schema
+let User = new Schema({
+   name: {
+      type: String
+   },
+   email: {
+      type: String
+   }
+}, {
+   collection: 'users'
+})
+module.exports = mongoose.model('User', User)
